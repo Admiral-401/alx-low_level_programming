396cb95b548663abca7317e0191d50698a31172e
Admiral-401 <ericmureithi401@gmail.com>
2023-06-16 19:08:56 -0700

    Bug fix

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+   char alphabets[] = "abcdefghijklmnopqrstuvwxyz";
+
+   for (int i = 0; i < 26; i++)
+   {
+   putchar(alphabets[i]);
+   }
+   putchar("\n");
+   return (0);
+}
+2-print_alphabet.c

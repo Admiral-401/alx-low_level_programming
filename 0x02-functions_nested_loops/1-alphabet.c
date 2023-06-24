cc3c4c457b27885bb520caf550c2c3773f15a7e0
Admiral-401 <ericmureithi401@gmail.com>
2023-06-24 01:57:41 -0700

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,13 +2,14 @@
 
 /**
  * print_alphabet - prints all alphabets in lowercase
+ *
+ * rturn (0); success
  */
 void print_alphabet(void)
 {
-    char ch;
+	char ch;
 
-    for (ch = 'a'; ch <= 'z'; ch++)
-        _putchar(ch);
-
-    _putchar('\n');
+	for (ch = 'a'; ch <= 'z'; ch++)
+	_putchar(ch);
+	_putchar('\n');
 }

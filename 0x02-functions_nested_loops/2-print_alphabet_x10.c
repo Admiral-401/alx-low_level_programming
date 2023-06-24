bf72ac4b988945affe8fdd616e1b8d4d143fec14
Admiral-401 <ericmureithi401@gmail.com>
2023-06-24 02:12:05 -0700

    2-print_alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,26 +3,23 @@
 /**
  * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
  * followed by a new line
+ * return (0); success
  */
 void print_alphabet_x10(void)
 {
-    char ch;
-    int i;
+	char ch;
+	int i;
 
-    i = 0;
-
-    while (i < 10)
-    {
-        ch = 'a';
-
-        while (ch <= 'z')
-        {
-            _putchar(ch);
-            ch++;
-        }
-
-        _putchar('\n');
-        i++;
-    }
+	i = 0;
+	while (i < 10)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		i++;
+	}
 }
-

aaf202a60187efc6e9799b2409a17ee614d75918
Admiral-401 <ericmureithi401@gmail.com>
2023-09-13 16:55:28 -0700

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ * @: takes no param
+ *
+ * Return: Nothing
+ */
+void print_numbers(void)
+{
+	unsigned int i;
+
+	for (i = 49; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}

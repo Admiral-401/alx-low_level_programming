ada833dbc1019485235e1c0eebc849b9065270e4
Admiral-401 <ericmureithi401@gmail.com>
2023-09-13 16:19:19 -0700

    initial commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,16 @@
-#include <unistd.h>
 #include "main.h"
 
 /**
  * _isupper - checks for uppercase character
  * @c: character to check
  *
- * return: 0 always success
+ * Return: 1 if c is uppercase 0 otherwise
  */
+
 int _isupper(int c)
 {
-	if (c == 65 && c <= 90)
-		return(1);
+	if (c >= 65 && c <= 90)
+		return (1);
 	else
 		return (0);
 }
